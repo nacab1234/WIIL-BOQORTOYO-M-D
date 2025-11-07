58bb81ff049f679ccd9c630281f95b09b4153ec3
WIIL BOQORTOYO M-D <nacabyaye@gmail.com>
2025-11-07 14:25:48 +0300

    Add initial settings configuration for the bot

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dullahxmd',
+  author: '‎',
+  botName: ""ᴅᴜʟʟᴀʜ-xᴍᴅ v²",
+  botOwner: 'WIIL BOQORTOYO-XMD', // Your name
+  ownerNumber: '252626799185', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.9",
+  updateZipUrl: "https://github.com/abdallahsalimjuma/DULLAH-XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
